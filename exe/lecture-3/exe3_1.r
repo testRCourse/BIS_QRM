a6b6804099e702114408a98db01a60625b66ae46
meikemorren <meikemorren@hotmail.com>
2016-01-29 15:43:16 +0100

    Create exe3_1.r

---
+++ exe/lecture-3/exe3_1.r
@@ -0,0 +1,25 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 3.1  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#################################################
+################# write function  ###############
+#################################################
+# calculate the means of the environmental variables
+# fist set the missing values to NA
+# 1. create function without loop (for each variable separately)
+
+
+
+
+
+# 2. do this using a for loop within function
+
+
+
+
+
+
+# 4. use a preprogrammed function colMeans
+# set missing values first to NA!
+
+
+
