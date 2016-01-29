3492903a63685dcc46ab968465e338b7de469ec6
meikemorren <meikemorren@hotmail.com>
2016-01-29 15:58:04 +0100

    Create exe3_2.r

---
+++ exe/lecture-3/exe3_2.r
@@ -0,0 +1,31 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 3.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#################################################
+#################### create for loop #############
+#################################################
+# You need to combine two datasets so that you
+# will attach the country names to the dataset
+
+# to do this, you will need to create a loop to 
+# a) read each country code in the dataset
+# b) read each country code in the file with country names
+
+
+
+# 1. read file with countrycodes : countrycodes.txt
+
+
+# 2. create loop to 'read' all countrycodes in dataset
+
+
+# 3. fetch all the rows with the first country code
+
+
+# 4. create loop to read the country codes in countrycodes.txt
+
+
+# 5. write the countrycode to the dataset 
+# for only those rows with that country code
+
+            
+
