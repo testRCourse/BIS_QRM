c7e59320b2b3dc752f76b78d54e0358db0d46cf3
meikemorren <meikemorren@hotmail.com>
2016-01-29 14:45:12 +0100

    Create exe2_2.r

---
+++ exe2_2.r
@@ -0,0 +1,45 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 2.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#########################################################
+##################### conduct MCAR test  #################
+#########################################################
+# you can find the entire function on github:
+# https://github.com/cran/BaylorEdPsych/blob/master/R/LittleMCAR.R
+# First install the package:
+
+# install accompagning package "mvnmle"
+
+
+# Then load the package into your environment so you can use the commands:
+
+
+# select a set of interval variables (likert scales included)
+# Little MCAR assumes multivariate normal distribution
+
+
+# conduct test
+# TIP: write results away immediately because test takes a long time to run
+
+
+# explore results by either $
+# or by []
+
+
+
+
+
+#########################################################
+##################### conduct MAR test  #################
+#########################################################
+# create a function to test for missingness on one variable
+# using a t-test at another set of (interval) variables
+
+# for instance education 
+
+# 1. create missingness variable
+
+
+
+# 2. t-test
+# use function you created before.
+# group is not the missingness variable
