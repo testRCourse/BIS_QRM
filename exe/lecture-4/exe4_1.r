00f88d52a3915013c72d885b78670eacb11dfb67
meikemorren <meikemorren@hotmail.com>
2016-01-29 16:30:47 +0100

    Update exe4_1.r

--- exe/lecture-4/exe4_1.r
+++ exe/lecture-4/exe4_1.r
@@ -1,1 +1,38 @@
-...
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 4.1  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#########################################################
+#################  data cleaning ########################
+#########################################################
+# read file
+
+
+
+#########################################################
+################# linear regression #####################
+#########################################################
+# 1. Write vectorised function of linear regression
+# using equations of b0 and b1
+
+
+
+
+
+# 2. write vectorized function of linear regression
+# using the sum of squares method
+
+
+
+# 3. check solutions with regular lm function
+
+
+
+#########################################################
+################# display residuals #####################
+#########################################################
+# make a plot of the x and y variable
+# ad regression line with b0 and b1 using 'abline'
+
+
+
+
+
