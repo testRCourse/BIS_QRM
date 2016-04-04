4decfc844b2849c1fb727a6d629b6e70a7754247
meikemorren <meikemorren@hotmail.com>
2016-04-04 14:44:26 +0200

    Update exe2_2.r

--- exe/lecture-2/exe2_2.r
+++ exe/lecture-2/exe2_2.r
@@ -1,21 +1,21 @@
-#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 2.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#$$$$$$$$$$$$$$$$$$$$$$   EXE 2.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
 #########################################################
 ##################### conduct MCAR test  #################
 #########################################################
-# you can find the entire function on github
+# you can find the entire function on github:
 # https://github.com/cran/BaylorEdPsych/blob/master/R/LittleMCAR.R
-# First install the package
+# First install the package:
 
 # install accompagning package "mvnmle"
 
 
-# Then load the package into your environment so you can use the commands
+# Then load the package into your environment so you can use the commands:
 
 
 # select a set of interval variables (likert scales included)
 # make sure that all the negative values are coded as NA
-# note that Little MCAR assumes multivariate normal distribution
+# Little MCAR assumes multivariate normal distribution
 
 
 # conduct test
