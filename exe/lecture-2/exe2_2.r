0236877b7bc4ffe46d53bc0b1292c4e4adf1a826
meikemorren <meikemorren@hotmail.com>
2016-04-04 15:32:41 +0200

    Update exe2_2.r

--- exe/lecture-2/exe2_2.r
+++ exe/lecture-2/exe2_2.r
@@ -1,4 +1,4 @@
-#$$$$$$$$$$$$$$$$$$$$$$   EXE 2.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 2.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
 #########################################################
 ##################### conduct MCAR test  #################
@@ -13,13 +13,24 @@
 # Then load the package into your environment so you can use the commands:
 
 
-# select a set of interval variables (likert scales included)
+# look for informaiton about function
+# Note that Little MCAR assumes multivariate normal distribution
+# i.e. this requires interval assumption
+
+
+
+# select a set of interval variables (minimum of 3, maximum of 10)
 # make sure that all the negative values are coded as NA
-# Little MCAR assumes multivariate normal distribution
+# and do not include variables that only have missing values!
+# select only one country so the function does not take too long
+# create a new matrix with these variables
+
 
 
 # conduct test
 # TIP: write results away immediately because test takes a long time to run
+
+
 
 
 # explore results by either $
@@ -36,8 +47,8 @@
 # using a t-test at another set of (interval) variables
 
 # for instance education 
-
 # 1. create missingness variable
+
 
 
 
