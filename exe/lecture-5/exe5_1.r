d775225b82160fb61313fd357ac0e913eea799b9
meikemorren <meikemorren@hotmail.com>
2016-04-07 17:38:04 +0200

    Create exe5_1.r

---
+++ exe/lecture-5/exe5_1.r
@@ -0,0 +1,49 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 5.1  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+
+#####################################################################
+####################### linear regression ###########################
+#####################################################################
+# 1. estimate a linear regression using the formulas discussed during 
+# the lecture. 
+
+# a. Write vectorised function of linear regression
+# using equations of b0 and b1
+# first listwise deletion
+
+
+
+
+#_________________________________________  OPTIONAL _______________________________________________________________#
+
+# b. find the fit of the model
+# using the sum of squares method
+
+
+
+
+#_________________________________________  END OPTIONAL _______________________________________________________________#
+
+# c. check solutions with regular lm function
+
+
+
+#####################################################################
+####################### dummy variable / t-test #####################
+#####################################################################
+# 2. estimate a model with only one dummy variable for country
+
+# a) create a dummy variable where the 1 indicates it is one country
+# and 0 indicates another country (you are free to select the countries)
+
+
+
+# b) include this variable into your model to explain satisfaction
+# you can use the lm function
+
+
+
+# c) compare with a t-test (using the function ?t.test) 
+
+
+
