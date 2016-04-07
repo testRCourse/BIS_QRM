e4b050b0de26fc34d0f56d5ec0814768416463e4
meikemorren <meikemorren@hotmail.com>
2016-04-07 17:39:30 +0200

    Create exe6_1.r

---
+++ exe/lecture-6/exe6_1.r
@@ -0,0 +1,27 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  EXE 6.1  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#########################################################
+################# test interaction effect  ##############
+#########################################################
+# first explore differences between countries in the two variables
+
+
+
+
+# a. calculate the interaction term 
+# add the interaction term to the independent variables
+
+
+
+
+
+
+# b. solve the multiple regression equation with interaction term
+# check with lm function
+# interpret the effects
+
+
+
+
+## c. assess model fit for multiple regression
+
