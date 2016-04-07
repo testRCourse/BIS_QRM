f4532f9c541624398b62f80c8dc2586e16d927a7
meikemorren <meikemorren@hotmail.com>
2016-04-07 17:25:21 +0200

    Added files via upload

---
+++ exe/lecture-4/exe4_2_BIS.R
@@ -0,0 +1,49 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 4.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#################################################
+#################### create for loop #############
+#################################################
+###### up to now we have worked with the WVS dataset
+###### with the country codes (i.e. numbers)
+###### we will now attach the country labels (i.e. names)
+###### to the WVS dataset
+###### for this exercise you need to combine two datasets 
+###### to attach the country names to the dataset
+
+# 1. create a loop to attach the country codes to the dataset
+# for this loop you need to select each country code in the dataset
+# and select the same country code in the file with country names
+# and connect these two together
+# to do this follow the following steps:
+
+
+# a) read file with countrycodes : countrycodes.txt
+
+
+
+# b) create loop to 'read' all countrycodes in dataset
+
+
+
+# c) fetch all the rows with the first countrycode
+# using an if loop
+
+
+
+# d) create loop to read the country codes in countrycodes.txt
+
+
+
+# e) write the country name associated with this
+# countrycode in countrycodes.txt to the dataset 
+# for only those rows with the same countrycode 
+
+            
+
+
+
+
+
+
+
+
