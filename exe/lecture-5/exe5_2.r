0900d091e0264bfaea0bca6583e823258563e882
meikemorren <meikemorren@hotmail.com>
2016-04-07 17:38:42 +0200

    Create exe5_2.r

---
+++ exe/lecture-5/exe5_2.r
@@ -0,0 +1,36 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  EXE 5.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#########################################################
+################# multiple regression #####################
+#########################################################
+## run multiple regression using the cost function
+# add country to the simple regression example 5.1 data
+# create dummy variable of country
+# use listwise deletion
+
+
+
+
+
+# create independent variable matrix with intercept
+# make sure the independent variables is a matrix, not list
+
+
+
+
+
+
+# solve equation
+# check with lm()
+
+
+
+
+#_________________________________________  OPTIONAL _______________________________________________________________#
+
+# calculate standardized coefficients
+
+
+
+
+
