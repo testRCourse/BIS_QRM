01661c09b230bf088a957a55580c029c025181c5
meikemorren <meikemorren@hotmail.com>
2016-04-07 17:41:01 +0200

    Create exe6_2.r

---
+++ exe/lecture-6/exe6_2.r
@@ -0,0 +1,25 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$  EXE 6.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+#########################################################
+##################### mean centering ####################
+#########################################################
+# a. center your independent variables around a meaningful value
+# use a for loop 
+
+
+
+
+# b. calculate the interaction term using the centered values
+# add the interaction term to the independent variables
+
+
+
+
+
+# c. conduct multiple regression to test interaction with lm function
+# interpret the effects
+# compare to uncentered solution and the centered solution without interaction term
+
+
+
+
+## d. assess model fit for multiple regression
