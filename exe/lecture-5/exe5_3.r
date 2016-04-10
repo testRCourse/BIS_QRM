764b8e250f997981cbcb8b86a44302a01ac9c589
meikemorren <meikemorren@hotmail.com>
2016-04-10 13:33:04 +0200

    Create exe5_3.r

---
+++ exe/lecture-5/exe5_3.r
@@ -0,0 +1,22 @@
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 5.3  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+
+#####################################################################
+####################### multiple regression ###########################
+#####################################################################
+# 3. standardize all variables
+
+
+
+
+# compare with the unstandardized model
+# plot the unstandardized and standardized model next to each other
+# for the first explanatory variable
+
+
+
+#______________________ OPTIONAL ______________________________#
+# include legend in the plot with the coefficient
+# change axis labels
+
