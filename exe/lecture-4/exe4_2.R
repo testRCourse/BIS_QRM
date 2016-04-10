e9bc1a3653b635d5841ac6760cc2de467e6498f6
meikemorren <meikemorren@hotmail.com>
2016-04-10 13:31:11 +0200

    Update exe4_2.R

--- exe/lecture-4/exe4_2.R
+++ exe/lecture-4/exe4_2.R
@@ -1,49 +1,38 @@
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 4.2  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
 
-#################################################
-#################### create for loop #############
-#################################################
-###### up to now we have worked with the WVS dataset
-###### with the country codes (i.e. numbers)
-###### we will now attach the country labels (i.e. names)
-###### to the WVS dataset
-###### for this exercise you need to combine two datasets 
-###### to attach the country names to the dataset
+#####################################################################
+########################### ordinal variables #######################
+#####################################################################
+# 1. estimate a model with one ordinal variable to explain 
+# satisfaction (you can choose from eduR or ageR)
 
-# 1. create a loop to attach the country codes to the dataset
-# for this loop you need to select each country code in the dataset
-# and select the same country code in the file with country names
-# and connect these two together
-# to do this follow the following steps:
-
-
-# a) read file with countrycodes : countrycodes.txt
+# a) include the variable as it is now in the model (so as numeric value,
+# if you already changed it to a factor variable, change it back to numeric
+# by as.numeric()
+# interpret the intercept and coefficient
 
 
 
-# b) create loop to 'read' all countrycodes in dataset
 
-
-
-# c) fetch all the rows with the first countrycode
-# using an if loop
-
-
-
-# d) create loop to read the country codes in countrycodes.txt
-
-
-
-# e) write the country name associated with this
-# countrycode in countrycodes.txt to the dataset 
-# for only those rows with the same countrycode 
-
-            
+# b) recode the variable so that the lower level is a zero
+# TIP create a new variable so you still have the original variable
+# compare the results with the one you obtained above
+# interpret the intercept and coefficient
 
 
 
 
 
+# c) estimate the model with the ordinal variable as a factor 
+# include the factor( ) command in the lm command
+# interpret the intercept and coefficient
 
 
+
+
+#______________________ OPTIONAL ______________________________#
+# d) plot the two variables using plot()
+# include the effect estimated with the lm function
+# TIP you need to include two lines! look at $coefficients
+# obtained with the lm function
 
