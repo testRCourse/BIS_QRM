6b70b32d38c663eec9f1fbb31bedb14b9a7e7619
meikemorren <meikemorren@hotmail.com>
2016-04-10 13:32:27 +0200

    Update exe5_1.r

--- exe/lecture-5/exe5_1.r
+++ exe/lecture-5/exe5_1.r
@@ -2,48 +2,15 @@
 
 
 #####################################################################
-####################### linear regression ###########################
+####################### multiple regression ###########################
 #####################################################################
-# 1. estimate a linear regression using the formulas discussed during 
-# the lecture. 
-
-# a. Write vectorised function of linear regression
-# using equations of b0 and b1
-# first listwise deletion
+# 1. estimate a regression equation with two independent interval 
+# or ordinal treated as interval variables
+# plot the line of the first variable
 
 
 
-
-#_________________________________________  OPTIONAL _______________________________________________________________#
-
-# b. find the fit of the model
-# using the sum of squares method
-
-
-
-
-#_________________________________________  END OPTIONAL _______________________________________________________________#
-
-# c. check solutions with regular lm function
-
-
-
-#####################################################################
-####################### dummy variable / t-test #####################
-#####################################################################
-# 2. estimate a model with only one dummy variable for country
-
-# a) create a dummy variable where the 1 indicates it is one country
-# and 0 indicates another country (you are free to select the countries)
-
-
-
-# b) include this variable into your model to explain satisfaction
-# you can use the lm function
-
-
-
-# c) compare with a t-test (using the function ?t.test) 
+# compare with a model that only includes one variable
 
 
 
