f8274e23b23f3c0e2ab672377816921c5a1b5a7f
meikemorren <meikemorren@hotmail.com>
2016-04-11 15:23:16 +0200

    Create exe1.r

---
+++ functions/exe1.r
@@ -0,0 +1,44 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$   EXE 4.1  $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$#
+
+#################################################
+################# write function  ###############
+#################################################
+###### a first exercise to learn how to write functions
+###### to let R do several operations at the same time
+###### for this exercise you need to select several
+###### columns in the WVS dataset. Do not select less than
+###### 5 but also not more than 
+###### 10 columns as this unnecessarily complicates
+###### the exercise.
+
+# 1. calculate the means of the variables
+# fist set the missing values to NA
+# a) create function without loop (for each variable separately)
+
+
+
+
+
+# b) do this using a for loop within function
+
+
+
+
+
+
+# c) use a preprogrammed function colMeans to 
+# calculate the means across a preselected set of
+# columns
+# set missing values first to NA!
+
+
+
+
+# d) use a preprogrammed function apply to 
+# calculate the means across a preselected set of
+# columns
+# use na.rm argument to deal with missings
+# look up function ?apply!
+
+
+
